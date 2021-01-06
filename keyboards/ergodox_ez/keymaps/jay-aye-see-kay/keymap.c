031c980599cb206970afdc0efaec15fcd7308651
Jack Rose <user@jackrose.co.nz>
2021-01-06 18:49:52 +1100

    use also tab as a layer 2 toggle
    - now I can access the arrow keys a little easier, this is what Gunz and
      Louis were doing so it must be a good idea

--- keyboards/ergodox_ez/keymaps/jay-aye-see-kay/keymap.c
+++ keyboards/ergodox_ez/keymaps/jay-aye-see-kay/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
   XXXXXXX,              KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,              XXXXXXX, KC_6,     KC_7,     KC_8,    KC_9,     KC_0,    XXXXXXX,
-  KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,              XXXXXXX, KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_BSLASH,
+  LT(MOVE, KC_TAB),     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,              XXXXXXX, KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_BSLASH,
   MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN, MT(MOD_RCTL, KC_QUOT),
   KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,              XXXXXXX, KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
   XXXXXXX,              XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT,                                      MO(CODE), MO(MOVE), XXXXXXX, XXXXXXX,  XXXXXXX,

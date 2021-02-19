3d505ff07b93ea9d86ba36819a2b55d6527f54f1
Jack Rose <user@jackrose.co.nz>
2021-02-19 13:52:48 +1100

    move control down to thumb to match laptop

--- keyboards/ergodox_ez/keymaps/jay-aye-see-kay/keymap.c
+++ keyboards/ergodox_ez/keymaps/jay-aye-see-kay/keymap.c
@@ -22,13 +22,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |Tab/MOVE|   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |Esc/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | '/Ctrl |
+ * | Esc    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Win  | Alt  |                                       | CODE | MOVE |      |      |      |
+ *   |      |      |  Alt | Win  | Ctl  |                                       | CODE | MOVE |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  XXXXXXX,              KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,              XXXXXXX, KC_6,     KC_7,     KC_8,    KC_9,     KC_0,    XXXXXXX,
-  LT(MOVE, KC_TAB),     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,              XXXXXXX, KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_BSLASH,
-  MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN, MT(MOD_RCTL, KC_QUOT),
-  KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,              XXXXXXX, KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
-  XXXXXXX,              XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI,                                      MO(CODE), MO(MOVE), XXXXXXX, XXXXXXX,  XXXXXXX,
-                                                             XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
-                                                                      KC_HOME,       KC_PGUP,
-                                                     KC_SPC, KC_BSPC,  KC_END,       KC_PGDN, KC_TAB, KC_ENT
+  XXXXXXX,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,              XXXXXXX, KC_6,     KC_7,     KC_8,    KC_9,     KC_0,    XXXXXXX,
+  LT(MOVE, KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,              XXXXXXX, KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_BSLASH,
+  KC_ESC,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN, KC_QUOT,
+  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,              XXXXXXX, KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
+  XXXXXXX,          XXXXXXX, KC_LALT, KC_LGUI, MOD_LCTL,                                     MO(CODE), MO(MOVE), XXXXXXX, XXXXXXX,  XXXXXXX,
+                                                         XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
+                                                                  KC_HOME,       KC_PGUP,
+                                                 KC_SPC, KC_BSPC,  KC_END,       KC_PGDN, KC_TAB, KC_ENT
 ),
 
 /* Keymap 1: Symbol Layer

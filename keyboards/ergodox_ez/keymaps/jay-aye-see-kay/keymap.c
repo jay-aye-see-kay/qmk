efea64cc6277c818a73fc4eb99d7044513cec9dd
Jack Rose <user@jackrose.co.nz>
2021-02-22 16:11:05 +1100

    switch from MOD to KC ctrl, former did not work

--- keyboards/ergodox_ez/keymaps/jay-aye-see-kay/keymap.c
+++ keyboards/ergodox_ez/keymaps/jay-aye-see-kay/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(MOVE, KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,              XXXXXXX, KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_BSLASH,
   KC_ESC,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN, KC_QUOT,
   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,              XXXXXXX, KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
-  XXXXXXX,          XXXXXXX, KC_LALT, KC_LGUI, MOD_LCTL,                                     MO(CODE), MO(MOVE), XXXXXXX, XXXXXXX,  XXXXXXX,
+  XXXXXXX,          XXXXXXX, KC_LALT, KC_LGUI, KC_LCTL,                                      MO(CODE), MO(MOVE), XXXXXXX, XXXXXXX,  XXXXXXX,
                                                          XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,
                                                                   KC_HOME,       KC_PGUP,
                                                  KC_SPC, KC_BSPC,  KC_END,       KC_PGDN, KC_TAB, KC_ENT

d36978007f178f282a2315c2372b03c8394da1c6
Jack Rose <user@jackrose.co.nz>
2021-03-22 10:55:24 +1100

    move backspace back to ergodox position
    - not sure about where the layer 1 key ended up, but it had to go
    somewhere

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/jay-aye-see-kay/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/jay-aye-see-kay/keymap.c
@@ -25,18 +25,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   OSL(_MIRROR),
    KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
    XXXXXXX,   KC_GRV,  XXXXXXX, KC_LALT,
-                               KC_LGUI,  KC_LCTL,
-                               KC_BSPC,  KC_SPC,
-                               MO(_MOVE),  MO(_CODE),
+                                           KC_LGUI,   KC_LCTL,
+                                           MO(_CODE), KC_SPC,
+                                           MO(_MOVE), KC_BSPC,
                         // right hand
-                         KC_5,         KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     XXXXXXX,
-                         XXXXXXX,      KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLASH,
-                         OSL(_MIRROR), KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                                       KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                                    KC_RALT,  KC_MINUS, KC_EQUAL, XXXXXXX,
-            KC_RCTL, KC_RGUI,
-            KC_ENTER, XXXXXXX,
-            MO(_CODE), MO(_MOVE)
+                         KC_5,         KC_6,  KC_7,  KC_8,    KC_9,     KC_0,     XXXXXXX,
+                         XXXXXXX,      KC_Y,  KC_U,  KC_I,    KC_O,     KC_P,     KC_BSLASH,
+                         OSL(_MIRROR), KC_H,  KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
+                                       KC_N,  KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
+                                                     KC_RALT, KC_MINUS, KC_EQUAL, XXXXXXX,
+                  KC_RCTL,  KC_RGUI,
+                  KC_ENTER, MO(_CODE),
+                  XXXXXXX,  MO(_MOVE)
 ),
 
 [_CODE] = LAYOUT_5x7(

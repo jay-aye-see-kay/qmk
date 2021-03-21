85fcdc6f3c12ce740c5427b04e18617c67c181a2
Jack Rose <user@jackrose.co.nz>
2021-03-21 17:37:00 +1100

    dactyl: create a mirror mode

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/jay-aye-see-kay/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/jay-aye-see-kay/keymap.c
@@ -8,6 +8,7 @@
 #define _QWERTY 0
 #define _CODE   1
 #define _MOVE   2
+#define _MIRROR 3
 
 // Some basic macros
 #define TASK    LCTL(LSFT(KC_ESC))
@@ -21,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
    XXXXXXX,   KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
    KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   XXXXXXX,
-   KC_ESC,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   XXXXXXX,
+   KC_ESC,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   OSL(_MIRROR),
    KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
    XXXXXXX,   KC_GRV,  XXXXXXX, KC_LALT,
                                KC_LGUI,  KC_LCTL,
                                KC_BSPC,  KC_SPC,
                                MO(_MOVE),  MO(_CODE),
-        // right hand
-                         KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     XXXXXXX,
-                         XXXXXXX, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLASH,
-                         XXXXXXX, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                                  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+                        // right hand
+                         KC_5,         KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     XXXXXXX,
+                         XXXXXXX,      KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLASH,
+                         OSL(_MIRROR), KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+                                       KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                                     KC_RALT,  KC_MINUS, KC_EQUAL, XXXXXXX,
             KC_RCTL, KC_RGUI,
             KC_ENTER, XXXXXXX,
@@ -57,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,
         _______, _______,
         _______, _______
-),
+  ),
 
 [_MOVE] = LAYOUT_5x7(
   // left hand
@@ -77,6 +78,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       _______,   _______,   _______,   _______,
         _______, _______,
         _______, _______,
-        _______, _______),
+        _______, _______
+  ),
 
+
+[_MIRROR] = LAYOUT_5x7(
+  // left hand
+   XXXXXXX,   KC_0,     KC_9,     KC_8,    KC_7,   KC_6,   KC_5,
+   KC_BSLASH, KC_P,     KC_O,     KC_I,    KC_U,   KC_Y,   _______,
+   KC_QUOT,   KC_SCLN,  KC_L,     KC_K,    KC_J,   KC_H,   _______,
+   KC_RSFT,   KC_SLSH,  KC_DOT,   KC_COMM, KC_M,   KC_N,
+   XXXXXXX,   KC_EQUAL, KC_MINUS, KC_RALT, 
+                               KC_RGUI,  KC_RCTL,
+                               KC_ENTER, XXXXXXX,
+                               MO(_MOVE),  MO(_CODE),
+                        // right hand
+                         KC_6,    KC_5,    KC_4,    KC_3,     KC_2,     KC_1,     XXXXXXX,
+                         _______, KC_T,    KC_R,    KC_E,     KC_W,     KC_Q,     KC_TAB,
+                         _______, KC_G,    KC_F,    KC_D,     KC_S,     KC_A,     KC_ESC,
+                                  KC_B,    KC_V,    KC_C,     KC_X,     KC_Z,     KC_LSFT,
+                                                    KC_LALT,  XXXXXXX,  KC_GRV,   XXXXXXX,
+            KC_LCTL, KC_LGUI,
+            KC_BSPC,  KC_SPC,
+            MO(_CODE), MO(_MOVE)
+  ),
 };

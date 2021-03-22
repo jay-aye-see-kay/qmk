7b038a5be15ddc8722303719425df023c1a049f3
Jack Rose <user@jackrose.co.nz>
2021-03-22 18:14:36 +1100

    try out some different layer key toggles
    - the bottom three on my thumb aren't really usable

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/jay-aye-see-kay/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/jay-aye-see-kay/keymap.c
@@ -20,23 +20,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_5x7(
   // left hand
-   XXXXXXX,   KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
-   KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   XXXXXXX,
-   KC_ESC,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   OSL(_MIRROR),
-   KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-   XXXXXXX,   KC_GRV,  XXXXXXX, KC_LALT,
-                                           KC_LGUI,   KC_LCTL,
-                                           MO(_CODE), KC_SPC,
-                                           MO(_MOVE), KC_BSPC,
+   XXXXXXX,   KC_1,    KC_2,     KC_3,     KC_4,   KC_5,   KC_6,
+   KC_TAB,    KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,   XXXXXXX,
+   KC_ESC,    KC_A,    KC_S,     KC_D,     KC_F,   KC_G,   OSL(_MIRROR),
+   KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,
+   XXXXXXX,   XXXXXXX, MO(_MOVE),MO(_CODE),
+                                           KC_LGUI, KC_LCTL,
+                                           KC_LALT, KC_SPC,
+                                           XXXXXXX, KC_BSPC,
                         // right hand
-                         KC_5,         KC_6,  KC_7,  KC_8,    KC_9,     KC_0,     XXXXXXX,
-                         XXXXXXX,      KC_Y,  KC_U,  KC_I,    KC_O,     KC_P,     KC_BSLASH,
-                         OSL(_MIRROR), KC_H,  KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
-                                       KC_N,  KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
-                                                     KC_RALT, KC_MINUS, KC_EQUAL, XXXXXXX,
-                  KC_RCTL,  KC_RGUI,
-                  KC_ENTER, MO(_CODE),
-                  XXXXXXX,  MO(_MOVE)
+                         KC_5,         KC_6,  KC_7,  KC_8,     KC_9,      KC_0,     XXXXXXX,
+                         XXXXXXX,      KC_Y,  KC_U,  KC_I,     KC_O,      KC_P,     KC_BSLASH,
+                         OSL(_MIRROR), KC_H,  KC_J,  KC_K,     KC_L,      KC_SCLN,  KC_QUOT,
+                                       KC_N,  KC_M,  KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,
+                                                     MO(_CODE),MO(_MOVE), XXXXXXX,  XXXXXXX,
+                  MO(_CODE),  KC_RGUI,
+                  KC_ENTER, KC_RALT,
+                  XXXXXXX,  XXXXXXX
 ),
 
 [_CODE] = LAYOUT_5x7(
